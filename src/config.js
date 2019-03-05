bbf68b50bcc706fba4cc4e836ada99e0d1774f61
mejison <mjs@div-art.com>
2019-03-05 20:24:28 +0200

    commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     api_url: 'https://api.thinger.io/v2',
-    device_id: 'esp8266',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiJlc3A4MjY2IiwiaWF0IjoxNTUxNTQwODM4LCJqdGkiOiI1YzdhYTI2NmJlMjQ4YWI3NjUzOGUxNDUiLCJ1c3IiOiJtZWppc29uIn0.PNKOzMzLXdwXLxEh0F3P4sSPwC9Z3BAAdIPzP0pV2RU'
+    device_id: 'enc28j60',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiJlbmMyOGo2MCIsImlhdCI6MTU1MTgxMDI1MCwianRpIjoiNWM3ZWJlY2E2YzA1OThhYjNiZTJkZmUwIiwidXNyIjoibWVqaXNvbiJ9._9G6CMal3130TWytKM1gvGtzRmqcKQEmH0oKhK8ak3M'
 }
